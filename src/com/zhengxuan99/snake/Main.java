e36b9bf126c627b5a92a91a3b8e84a53d9600095
RENOLEVES <zzhao260985652@gmail.com>
2021-08-25 13:13:28 -0400

    8/24

--- src/com/zhengxuan99/snake/Main.java
+++ src/com/zhengxuan99/snake/Main.java
@@ -3,6 +3,13 @@ package com.zhengxuan99.snake;
 public class Main {
     public static void main(String[] args) {
         SnakeFrame sf = new SnakeFrame();
-        sf.repaint();
+        while (true) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            sf.repaint();
+        }
     }
 }
--- src/com/zhengxuan99/snake/SnakeFrame.java
+++ src/com/zhengxuan99/snake/SnakeFrame.java
@@ -1,23 +1,26 @@
 package com.zhengxuan99.snake;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class SnakeFrame extends Frame {
     static final int GAME_WIDTH = 500, GAME_HEIGHT = 500;
 
-    Snake snake = new Snake(50,50,Dir.DOWN);
+    Snake snake = new Snake(50, 50, Dir.DOWN);
+
+    Food food = new Food(80,80,20,20);
     Dimension ScreenSize = getToolkit().getScreenSize();
     int ScreenWidth = (int) ScreenSize.getWidth();
     int ScreenHeight = (int) ScreenSize.getHeight();
-
 
     public SnakeFrame() {
         setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("Snake");
-        setLocation(ScreenWidth/2-250,ScreenHeight/2-250);
+        setLocation(ScreenWidth / 2 - 250, ScreenHeight / 2 - 250);
         setVisible(true);
 
         addKeyListener(new MyKeyListener());
@@ -41,7 +44,7 @@ public class SnakeFrame extends Frame {
     }
 
 
-    private class MyKeyListener implements KeyListener {
+    private class MyKeyListener extends KeyAdapter {
 
         boolean bU = false;
         boolean bD = false;
@@ -49,60 +52,182 @@ public class SnakeFrame extends Frame {
         boolean bR = false;
 
         @Override
-        public void keyTyped(KeyEvent e) {}
-
-        @Override
         public void keyPressed(KeyEvent e) {
-            int Key =e.getKeyCode();
+            int Key = e.getKeyCode();
 
-            switch (Key){
+            switch (Key) {
                 case KeyEvent.VK_UP:
                     bU = true;
+                    bD=bL=bR=false;
+                    break;
                 case KeyEvent.VK_DOWN:
                     bD = true;
+                    bU=bL=bR=false;
+                    break;
                 case KeyEvent.VK_LEFT:
                     bL = true;
+                    bD=bU=bR=false;
+                    break;
                 case KeyEvent.VK_RIGHT:
                     bR = true;
+                    bD=bL=bU=false;
+                    break;
             }
+
+            setMainDirection();
+        }
+
+        private void setMainDirection() {
+            if (!bU && !bD && !bL && !bR) snake.setMoving(false);
+            else {
+                snake.setMoving(true);
+                if (bL) snake.setDir(Dir.LEFT);
+                if (bU) snake.setDir(Dir.UP);
+                if (bR) snake.setDir(Dir.RIGHT);
+                if (bD) snake.setDir(Dir.DOWN);
+            }
+
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-
         }
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
 
         snake.paint(g);
+        food.paint(g);
     }
 }
 
-enum Dir{UP,DOWN,LEFT,RIGHT}
+enum Dir {UP, DOWN, LEFT, RIGHT}
 
-class Snake{
-    int x,y=50;
-    static final int WIDTH = 30, HEIGHT = 30;
+class Snake {
+    int x, y = 50;
+    private static final int WIDTH = 30, HEIGHT = 30;
+    private static final int SPEED = 10;
     Dir dir = Dir.RIGHT;
     Rectangle rect = new Rectangle();
 
-    public Snake(int x,int y, Dir dir) {
+    private boolean moving = false;
+
+    boolean ismoving() { return moving; }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public Dir getDir() {
+        return dir;
+    }
+
+    public void setDir(Dir dir) {
+        this.dir = dir;
+    }
+
+    public Snake(int x, int y, Dir dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
-//        this.rect = rect;
-//
-//        rect.x = x;
-//        rect.y = y;
-//        rect.width = WIDTH;
-//        rect.height = HEIGHT;
     }
 
-    public void paint(Graphics g){
+    public void move() {
+        if (!moving) System.out.println(moving);
+
+        else {
+            switch (dir) {
+                case UP:
+                    y -= SPEED;
+                    break;
+                case DOWN:
+                    y += SPEED;
+                    break;
+                case LEFT:
+                    x -= SPEED;
+                    break;
+                case RIGHT:
+                    x += SPEED;
+                    break;
+            }
+        }
+    }
+
+    public void paint(Graphics g) {
         Color c = g.getColor();
         g.setColor(Color.BLACK);
         g.setColor(c);
-        g.fillOval(x,y,WIDTH,HEIGHT);
+        g.fillOval(x, y, WIDTH, HEIGHT);
+
+        move();
     }
+}
+
+class Food{
+    Random random = new Random();
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWIDTH() {
+        return WIDTH;
+    }
+
+    public void setWIDTH(int WIDTH) {
+        this.WIDTH = WIDTH;
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public void setHEIGHT(int HEIGHT) {
+        this.HEIGHT = HEIGHT;
+    }
+
+    private int y;
+    private int WIDTH,HEIGHT = 6;
+    private boolean exist = true;
+
+    public Food(int x, int y, int WIDTH, int HEIGHT) {
+        this.x = x;
+        this.y = y;
+        this.WIDTH = WIDTH;
+        this.HEIGHT = HEIGHT;
+    }
+
+
+    private void spawn(){
+        if(!exist) {
+            int r1 = random.nextInt(500);
+            int r2 = random.nextInt(500);
+            x = r1;
+            y = r2;
+        }
+        else return;
+    }
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.YELLOW);
+        g.setColor(c);
+        g.fillOval(x, y, WIDTH, HEIGHT);
+
+        spawn();
+    }
+
 }
